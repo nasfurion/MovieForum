91e8960d1b96371de1bd42ec0a61702660bf559f
nasfurion <you@example.com>
2025-02-09 18:51:26 -0400

    Assignment completed

---
+++ MovieForum/Models/Discussion.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieForum.Models
+{
+    public class Discussion
+    {
+        // Primary key
+        public int DiscussionId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string? ImageFilename { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        // Property for file upload
+        [NotMapped]
+        [Display(Name = "Photo")]
+        public IFormFile? ImageFile { get; set; }
+
+        // Navigation Property
+        public List<Comment>? Comments { get; set; }
+    }
+}
